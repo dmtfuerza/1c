5d8a959e45520b7c27edd8bd35eb223f89c3a62d
Dominic Fuerza <dmtfuerza@gmail.com>
2013-06-24 16:59:11 -0400

    1

---
+++ js/_vti_cnf/jquery.easing.1.3.js
@@ -0,0 +1,11 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|23 Jun 2013 00:37:52 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|contact.html services.html about.html index.html
+vti_author:SR|basement\\mtigue
+vti_modifiedby:SR|basement\\mtigue
+vti_timecreated:TR|23 Jun 2013 00:37:52 -0000
+vti_cacheddtm:TX|23 Jun 2013 00:37:52 -0000
+vti_filesize:IR|8097
+vti_syncwith_68.178.246.6\:21/dominic/app1:TR|23 Jun 2013 00:37:52 -0000
+vti_syncofs_68.178.246.6\:21/dominic/app1:TW|24 Jun 2013 14:53:22 -0000
